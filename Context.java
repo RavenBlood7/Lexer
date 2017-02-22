32446fd69761c42167e24298747dc71b8e14cd56
RavenBlood7 <juan.dupreez82@gmail.com>
2017-02-22 12:39:22 +0200

    a better understanding

--- Context.java
+++ Context.java
@@ -1,4 +1,40 @@
 public class Context
 {
+	private int[][] transitionTable;
+	private boolean[] accepting;
+	private String[] description;
 	
+	Context()
+	{
+		transitionTable = new int[85][59];
+		stateTable = new boolean[85];
+		description = new String[85];
+	}
+	
+	public int getNextState(int state, char x)
+	{
+		return transitionTable[state][getCharIndex(x)];
+	}
+	
+	public String getDescription(int state)
+	{
+		return description[state];
+	}
+	
+	public boolean isAccepting(int state)
+	{
+		return accepting[state];
+	}
+	
+	public int isKeyword(String word)
+	{
+		//returns -1 if it is not keword
+		//else returns the state
+	}
+	
+	//helper functions
+	private int getCharIndex(char x)
+	{
+		
+	}
 }
--- Lexer.java
+++ Lexer.java
@@ -1,4 +1,7 @@
 public class Lexer
 {
+	private Context context;
+	
+	
 	
 }
